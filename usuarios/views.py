7ffdb79a6e089f8d7c8cfed3018acd0bc08e6410
Yhago Felipe <yhago.felipe.teles@gmail.com>
2024-01-15 14:47:42 -0300

    Login e Logout view

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -26,8 +27,30 @@ def cadastro(request):
                 username=username,
                 password=senha
             )
-            return redirect ('/usuarios/login')
+            return redirect ('/usuarios/logar')
         except:
             messages.add_message(request, constants.ERROR, 'Desculpe, nosso servidor está passando por momentos difíceis')
             return redirect('/usuarios/cadastro')
+        
 
+def logar(request):
+    if request.method == "GET":
+        return render(request,'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            messages.add_message(request, constants.SUCCESS, 'Parabéns! Conta Logada com Sucesso.')
+            return redirect('/fleshstudy/novo_fleshstudy/')
+        else:
+            messages.add_message(request, constants.ERROR, 'Username ou Senha errado! Por favor, tente novamente.')
+            return redirect('/usuarios/logar/')
+        
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/usuarios/logar')
