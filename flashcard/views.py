c6bd3c9e7eadd1e5d35914c00f3f25b83ba68b9d
Yhago Felipe <yhago.felipe.teles@gmail.com>
2024-01-22 11:29:15 -0300

    Criação do relatorio

--- flashcard/views.py
+++ flashcard/views.py
@@ -125,3 +125,21 @@ def responder_flashcard(request, id):
  flashcard_desafio.acertou = True if acertou == '1' else False
  flashcard_desafio.save()
  return redirect(f'/flashcard/desafio/{desafio_id}/')
+
+
+def relatorio(request,id):
+    desafio = Desafio.objects.get(id=id)
+
+    acertos = desafio.flashcards.filter(acertou=True).count()
+    erros = desafio.flashcards.filter(acertou=False).count()
+
+    dados = [acertos, erros]
+
+    categorias = desafio.categoria.all()
+    name_categoria = [i.nome for i in categorias]
+
+    dados2 = []
+    for categoria in categorias:
+        dados2.append(desafio.flashcards.filter(flashcard__categoria=categoria).filter(acertou=True).count())
+
+    return render(request, 'relatorio.html', {'desafio': desafio, 'dados': dados, 'categorias': name_categoria, 'dados2': dados2,},)
